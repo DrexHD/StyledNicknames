d927480f6196bc3fd3e7b1152ca059c92394e17d
Drex <nicknamedrex@gmail.com>
2023-08-09 16:13:05 +0200

    Update to 23w31a

--- src/main/java/eu/pb4/stylednicknames/mixin/PlayerManagerMixin.java
+++ src/main/java/eu/pb4/stylednicknames/mixin/PlayerManagerMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
     @Inject(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;sendCommandTree(Lnet/minecraft/server/network/ServerPlayerEntity;)V", shift = At.Shift.AFTER))
-    private void styledNicknames$loadNickname(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+    private void styledNicknames$loadNickname(ClientConnection connection, ServerPlayerEntity player, int latency, CallbackInfo ci) {
         NicknameHolder.of(player).styledNicknames$loadData();
     }
 }
